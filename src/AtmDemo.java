90f61d4e3c1286b60b87ebb0656c7910866a6de6
Merlin Jone J <62302915+Merlinjone@users.noreply.github.com>
2023-06-13 00:21:53 -0700

    AtmDemo.java

--- src/AtmDemo.java
+++ src/AtmDemo.java
@@ -1,5 +1,12 @@
-public class Main {
+import java.lang.String;
+import java.util.*;
+public class AtmDemo {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        atm Atm=new atm("Merlin",1234);
+        Atm.displayDetails();
+        }
+}
 
-    }
+}
 }
